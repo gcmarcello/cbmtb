7bbed05f3e5c9f755a252ede7f9da54ce27b052a
gcmarcello <marcellogcoelho@outlook.com>
2023-03-05 15:59:52 -0300

    testing email cors

--- routes/emails.js
+++ routes/emails.js
@@ -1,62 +1,13 @@
 const router = require("express").Router();
 const pool = require("../database");
 const adminAuthorization = require("./middlewares/adminAuthorization");
-const { listVerifiedEmails } = require("../apis/awsSES");
+const cors = require("cors");
 
-// List Verified Emails
-router.get("/", adminAuthorization, async (req, res) => {
-  try {
-    const list = await listVerifiedEmails();
-    return res.json(list);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
+const sgMail = require("@sendgrid/mail");
 
 router.post("/recovery/:id", adminAuthorization, async (req, res) => {
   try {
-    const list = await listVerifiedEmails();
     return res.json(list);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-
-router.post("/bounces/", async (req, res) => {
-  try {
-    console.log(req.body);
-    const messageObj = JSON.parse(req.body.Message);
-    const notificationType = messageObj.notificationType;
-    const emailDestination = messageObj.mail.destination[0];
-    const bounceType = messageObj.bounce.bounceType;
-    const emailSource = messageObj.mail.source;
-
-    if (emailSource === "newsletter@cbmtb.com" && bounceType === "Permanent") {
-      const deleteRecipient = await pool.query("DELETE FROM newsletter_recipients WHERE recipient_email = $1", [emailDestination]);
-    }
-    if (emailSource === "noreply@cbmtb.com" && bounceType === "Permanent") {
-      const deleteRecipient = await pool.query("DELETE FROM users WHERE user_email = $1", [emailDestination]);
-    }
-    return res.json(req.body);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-
-router.post("/complaints/", async (req, res) => {
-  try {
-    console.log(req.body);
-    const messageObj = JSON.parse(req.body.Message);
-    const notificationType = messageObj.notificationType;
-    const emailDestination = messageObj.mail.destination[0];
-    const bounceType = messageObj.bounce.bounceType;
-    const emailSource = messageObj.mail.source;
-
-    if (emailSource === "newsletter@cbmtb.com") {
-      const deleteRecipient = await pool.query("DELETE FROM newsletter_recipients WHERE recipient_email = $1", [emailDestination]);
-    }
-
-    return res.json(req.body);
   } catch (err) {
     console.log(err.message);
   }
@@ -72,6 +23,32 @@ router.post("/newsletter/", async (req, res) => {
       return res.status(400).json({ message: `Erro ao se inscrever na newsletter. Este email já existe.`, type: "error" });
     }
 
+    return res.status(200).json({ message: "Inscrição realizada com sucesso!. Você receberá uma confirmação por e-mail.", type: "success" });
+  } catch (err) {
+    return res.status(400).json({ message: `Erro ao se inscrever na newsletter. ${err.message}`, type: "error" });
+  }
+});
+
+router.post("/teste/", cors({ origin: "http://example.com" }), async (req, res) => {
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: "marcellog.c@hotmail.com", // Change to your recipient
+      from: {
+        name: "CBMTB - Confederação Brasileira de Mountain Bike",
+        email: "noreply@cbmtb.com",
+      }, // Change to your verified sender
+      subject: "Email teste confirmação de conta",
+      html: "<h2>Confirmação de Criação de Conta</h2><p>Prezado(a) [NOME DO USUÁRIO],</p><p>Obrigado por criar uma conta em nosso site! Sua conta foi criada com sucesso e agora você pode aproveitar todos os recursos que oferecemos.</p><p>Para começar, por favor confirme o seu endereço de e-mail clicando no link abaixo:</p><p><a href='#'>Confirmar Conta</a></p><p>Se você não criou esta conta, por favor, ignore este e-mail.</p><p>Obrigado mais uma vez por se inscrever em nosso site. Estamos ansiosos para vê-lo(a) lá!</p><br><p>Atenciosamente,</p><p>[A EQUIPE DO SITE]</p>",
+    };
+    /* sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      }); */
     return res.status(200).json({ message: "Inscrição realizada com sucesso!. Você receberá uma confirmação por e-mail.", type: "success" });
   } catch (err) {
     return res.status(400).json({ message: `Erro ao se inscrever na newsletter. ${err.message}`, type: "error" });
