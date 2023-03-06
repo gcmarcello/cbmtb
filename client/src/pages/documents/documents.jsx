a6a0939a709fa8d6eda896c53d70e844e60c91ee
gcmarcello <marcellogcoelho@outlook.com>
2023-03-06 15:16:36 -0300

    fix: bug where year accordion wouldnt open up

--- client/src/pages/documents/documents.jsx
+++ client/src/pages/documents/documents.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { toast } from "react-toastify";
 
 const Documents = () => {
@@ -14,7 +14,8 @@ const Documents = () => {
       });
       const parseResponse = await response.json();
       setDocuments(parseResponse);
-      setDocumentYears(...new Set([parseResponse.map((document) => document.document_year)]));
+      const years = parseResponse.map((document) => document.document_year);
+      setDocumentYears([...new Set(years)]);
     } catch (err) {
       console.log(err);
     } finally {
@@ -48,92 +49,52 @@ const Documents = () => {
   }, []);
 
   return (
-    <div className="container inner-page">
-      <h1>Transparência</h1>
-      <div className="mx-2">
-        <p className="text-justify">
-          Bem-vindo à página de Transparência da CBMTB! Aqui você poderá acessar nossos estatutos, regulamentos, atas de reuniões e outros documentos
-          importantes. Também fornecemos informações sobre nossa estrutura organizacional, incluindo os membros de nosso conselho e equipe
-          administrativa. Além disso, nossa página de Transparência oferece informações sobre nosso orçamento, finanças e investimentos.
-        </p>
-        <p className="text-justify">
-          Publicamos anualmente nossos balanços financeiros, demonstrações de resultados e relatórios de auditoria, para que nossos membros possam
-          acompanhar o desempenho financeiro de nossa entidade. Estamos comprometidos em manter um alto nível de integridade e ética em nossas
-          operações. Como tal, nossa página de Transparência e Documentação é atualizada regularmente e estamos sempre disponíveis para responder a
-          quaisquer dúvidas ou preocupações que nossos membros possam ter.
-        </p>
-      </div>
-
-      <div className="accordion" id="accordionExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingOne">
-            <button
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >
-              Documentação Geral
-            </button>
-          </h2>
-          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <div className="list-group">
-                {documents
-                  .filter((document) => document.document_general === true)
-                  .map((document) => (
-                    <button
-                      key={document.document_id}
-                      className="list-group-item list-group-item-action"
-                      aria-current="true"
-                      onClick={(e) => openDocument(e, document.document_link.split("/").pop())}
-                    >
-                      <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">{document.document_title}</h5>
-                        <small>{document.document_year}</small>
-                      </div>
-                      <p className="mb-1">{document.document_description}</p>
-                      {/* <small>And some small print.</small> */}
-                    </button>
-                  ))}
-              </div>
-            </div>
-          </div>
+    <Fragment>
+      <div className="container inner-page">
+        <h1>Transparência</h1>
+        <div className="mx-2">
+          <p className="text-justify">
+            Bem-vindo à página de Transparência da CBMTB! Aqui você poderá acessar nossos estatutos, regulamentos, atas de reuniões e outros
+            documentos importantes. Também fornecemos informações sobre nossa estrutura organizacional, incluindo os membros de nosso conselho e
+            equipe administrativa. Além disso, nossa página de Transparência oferece informações sobre nosso orçamento, finanças e investimentos.
+          </p>
+          <p className="text-justify">
+            Publicamos anualmente nossos balanços financeiros, demonstrações de resultados e relatórios de auditoria, para que nossos membros possam
+            acompanhar o desempenho financeiro de nossa entidade. Estamos comprometidos em manter um alto nível de integridade e ética em nossas
+            operações. Como tal, nossa página de Transparência e Documentação é atualizada regularmente e estamos sempre disponíveis para responder a
+            quaisquer dúvidas ou preocupações que nossos membros possam ter.
+          </p>
         </div>
-      </div>
-      <div className="accordion mt-2" id="accordionExample">
-        {documentYears.map((year) => (
-          <div className="accordion-item" key={year}>
-            <h2 className="accordion-header" id="headingTwo">
+
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
               <button
-                className="accordion-button collapsed"
+                className="accordion-button"
                 type="button"
                 data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
               >
-                Documentos {year}
+                Documentação Geral
               </button>
             </h2>
-            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
               <div className="accordion-body">
                 <div className="list-group">
                   {documents
-                    .filter((document) => document.document_year === year && !document.document_general)
+                    .filter((document) => document.document_general === true)
                     .map((document) => (
                       <button
                         key={`${document.document_id}-general`}
-                        href="#"
                         className="list-group-item list-group-item-action"
                         aria-current="true"
                         onClick={(e) => openDocument(e, document.document_link.split("/").pop())}
                       >
                         <div className="d-flex w-100 justify-content-between">
                           <h5 className="mb-1">{document.document_title}</h5>
-                          {/* <small>3 days ago</small> */}
+                          <small>{document.document_year}</small>
                         </div>
                         <p className="mb-1">{document.document_description}</p>
                         {/* <small>And some small print.</small> */}
@@ -143,53 +104,57 @@ const Documents = () => {
               </div>
             </div>
           </div>
-        ))}
-      </div>
-      {/* <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any
-              HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any
-              HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+
+        <div className="accordion mt-2" id={`accordion-years`}>
+          {documentYears.map((year) => (
+            <div className="accordion-item" key={year}>
+              <h2 className="accordion-header" id={`heading-${year}`}>
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse-${year}`}
+                  aria-expanded="false"
+                  aria-controls={`heading-${year}`}
+                >
+                  Documentos {year}
+                </button>
+              </h2>
+              <div
+                id={`collapse-${year}`}
+                className="accordion-collapse collapse"
+                aria-labelledby={`heading-${year}`}
+                data-bs-parent={`#accordion-years`}
+              >
+                <div className="accordion-body">
+                  <div className="list-group">
+                    {documents
+                      .filter((document) => document.document_year === year && !document.document_general)
+                      .map((document) => (
+                        <button
+                          key={document.document_id}
+                          href="#"
+                          className="list-group-item list-group-item-action"
+                          aria-current="true"
+                          onClick={(e) => openDocument(e, document.document_link.split("/").pop())}
+                        >
+                          <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">{document.document_title}</h5>
+                            {/* <small>3 days ago</small> */}
+                          </div>
+                          <p className="mb-1">{document.document_description}</p>
+                          {/* <small>And some small print.</small> */}
+                        </button>
+                      ))}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div> */}
-    </div>
+          ))}{" "}
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
