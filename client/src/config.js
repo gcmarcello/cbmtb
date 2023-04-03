b96d3ff3ad71c1a77ed86c895f1b8cd4d07be677
fernando <fgcoelho1709@outlook.com>
2023-04-03 19:03:13 -0300

    feat: support email

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,7 @@ const config = {
   logoUrl: "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logonotxt.png",
   entidade: "CBMTB",
   entidade_fullName: "Confederação Brasileira de Mountain Bike",
+  supportEmail: "suporte@cbmtb.com",
 };
 
 export default config;
