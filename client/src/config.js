41fc14102cf53ce3ecaa9441106e26db342f5dc2
fernando <fgcoelho1709@outlook.com>
2023-04-03 22:54:19 -0300

    feat: new global configs

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,19 @@
 const config = {
+  siteUrl: "cbmtb.com",
   logoUrl: "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logonotxt.png",
-  entidade: "CBMTB",
-  entidade_fullName: "Confederação Brasileira de Mountain Bike",
-  supportEmail: "suporte@cbmtb.com",
+  entidade: {
+    abbreviation: "CBMTB",
+    name: "Confederação Brasileira de Mountain Bike",
+    type: "Confederação",
+  },
+  contact: {
+    supportEmail: "suporte@cbmtb.com",
+  },
+  redes: {
+    twitter: "https://twitter.com/cbmtb",
+    instagram: "https://www.instagram.com/cbmtb/",
+    facebook: "https://pt-br.facebook.com/confederacaobrasileiramtb/",
+  },
 };
 
 export default config;
