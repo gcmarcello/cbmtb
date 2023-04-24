aaf8c3991154d7217ae13575de0afc8edbdee338
gcmarcello <marcellogcoelho@outlook.com>
2023-04-24 12:02:16 -0300

    feat: start overhauling registration page

--- client/src/pages/registration/components/userInfo.jsx
+++ client/src/pages/registration/components/userInfo.jsx
@@ -1,7 +1,27 @@
-import { React, Fragment } from "react";
+import { React, Fragment, useEffect } from "react";
 import InputMask from "react-input-mask";
 
-const UserInfo = ({ user, registrationInfo, setRegistrationInfo }) => {
+const dayjs = require("dayjs");
+
+const UserInfo = (props) => {
+  useEffect(() => {
+    if (props.user) {
+      props.setValue("firstName", props.user.user_first_name);
+      props.setValue("lastName", props.user.user_last_name);
+      props.setValue("email", props.user.user_email);
+      props.setValue("cpf", props.user.user_cpf);
+      props.setValue("phone", props.user.user_phone);
+      props.setValue("gender", props.user.user_gender);
+      props.setValue("birthDate", dayjs(props.user.user_birth_date).format("YYYY-MM-DD"));
+      props.setValue("cep", props.user.user_cep);
+      props.setValue("state", props.user.user_state);
+      props.setValue("city", props.user.user_city);
+      props.setValue("address", props.user.user_address);
+      props.setValue("number", props.user.user_number);
+      props.setValue("apartment", props.user.user_apartment);
+    }
+  }, [props.user, props.setValue, props]);
+
   return (
     <Fragment>
       <form className="mb-3">
@@ -17,8 +37,7 @@ const UserInfo = ({ user, registrationInfo, setRegistrationInfo }) => {
               id="user_given_name"
               name="user_given_name"
               className="form-control"
-              value={user.user_first_name}
-              onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
+              {...props.register("firstName", { required: true, pattern: /^([A-Za-z]+\s*){3,}$/i })}
             />
             <label htmlFor="user_last_name">Sobrenome</label>
             <input
@@ -26,50 +45,30 @@ const UserInfo = ({ user, registrationInfo, setRegistrationInfo }) => {
               id="user_last_name"
               name="user_last_name"
               className="form-control"
-              value={user.user_last_name}
-              onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
+              {...props.register("lastName", { required: true, pattern: /^([A-Za-z]+\s*){3,}$/i })}
             />
-            {
-              <div className="row">
-                <div className="col-12 col-lg-6">
-                  <label htmlFor="user_gender">Sexo</label>
-                  <select type="text" id="user_gender" name="user_gender" className="form-select" defaultValue={user.user_gender} disabled>
-                    <option value="" disabled={true}>
-                      Selecione
-                    </option>
-                    <option value={"Masculino"}>Masculino</option>
-                    <option value={"Feminino"}>Feminino</option>
-                  </select>
-                </div>
-                <div className="col-12 col-lg-6">
-                  <label htmlFor="user_cpf">CPF</label>
-                  <input type="text" id="user_cpf" name="user_cpf" className="form-control" defaultValue={user.user_cpf} disabled />
-                </div>
+            <div className="row">
+              <div className="col-12 col-lg-6">
+                <label htmlFor="user_gender">Sexo</label>
+                <select type="text" id="user_gender" name="user_gender" className="form-select" defaultValue={""} disabled>
+                  <option value="" disabled={true}>
+                    {props.user?.user_gender}
+                  </option>
+                </select>
               </div>
-            }
+              <div className="col-12 col-lg-6">
+                <label htmlFor="user_cpf">CPF</label>
+                <input type="text" id="user_cpf" name="user_cpf" className="form-control" defaultValue={props.user?.user_cpf} disabled />
+              </div>
+            </div>
             <div className="row">
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_phone">Celular</label>
-                <InputMask
-                  type="text"
-                  id="user_phone"
-                  name="user_phone"
-                  className="form-control"
-                  value={user.user_phone}
-                  mask="(99) 99999-9999"
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <InputMask type="text" id="user_phone" name="user_phone" className="form-control" mask="(99) 99999-9999" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_birth_date">Data de Nascimento</label>
-                <input
-                  type="date"
-                  id="user_birth_date"
-                  name="user_birth_date"
-                  className="form-control"
-                  defaultValue={user.user_formattedDate}
-                  disabled
-                />
+                <input type="date" id="user_birth_date" name="user_birth_date" className="form-control" disabled />
               </div>
             </div>
           </div>
@@ -78,80 +77,31 @@ const UserInfo = ({ user, registrationInfo, setRegistrationInfo }) => {
           </div>
           <div className="col-12 col-lg-5">
             <label htmlFor="user_cep">CEP</label>
-            <input
-              type="text"
-              id="user_cep"
-              name="user_cep"
-              className="form-control"
-              value={user.user_cep}
-              onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-            />
+            <input type="text" id="user_cep" name="user_cep" className="form-control" />
             <div className="row">
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_state">Estado</label>
-                <input
-                  type="text"
-                  id="user_state"
-                  name="user_state"
-                  className="form-control"
-                  value={user.user_state}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_state" name="user_state" className="form-control" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_city">Cidade</label>
-                <input
-                  type="text"
-                  id="user_city"
-                  name="user_city"
-                  className="form-control"
-                  value={user.user_city}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_city" name="user_city" className="form-control" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_street">Endereço</label>
-                <input
-                  type="text"
-                  id="user_street"
-                  name="user_street"
-                  className="form-control"
-                  value={user.user_street}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_street" name="user_street" className="form-control" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_number">Número</label>
-                <input
-                  type="text"
-                  id="user_number"
-                  name="user_number"
-                  className="form-control"
-                  value={user.user_number}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_number" name="user_number" className="form-control" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_neighborhood">Bairro</label>
-                <input
-                  type="text"
-                  id="user_neighborhood"
-                  name="user_neighborhood"
-                  className="form-control"
-                  value={user.user_neighborhood}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_neighborhood" name="user_neighborhood" className="form-control" />
               </div>
               <div className="col-12 col-lg-6">
                 <label htmlFor="user_apartment">Complemento</label>
-                <input
-                  type="text"
-                  id="user_apartment"
-                  name="user_apartment"
-                  className="form-control"
-                  value={user.user_apartment}
-                  onChange={(e) => setRegistrationInfo({ ...registrationInfo, user: { ...user, [e.target.name]: e.target.value } })}
-                />
+                <input type="text" id="user_apartment" name="user_apartment" className="form-control" />
               </div>
             </div>
           </div>
--- client/src/pages/registration/registration.jsx
+++ client/src/pages/registration/registration.jsx
@@ -1,7 +1,6 @@
 // Library Components
 import React, { useState, useEffect, Fragment } from "react";
 import { useParams, useNavigate, Navigate } from "react-router-dom";
-import { fetchInformation } from "./functions/fetchRegistrationInfo";
 import { toast } from "react-toastify";
 
 // General Components
@@ -12,57 +11,91 @@ import UserInfo from "./components/userInfo";
 import EventInfo from "./components/eventInfo";
 import StageButtons from "./components/stageButtons";
 import Payments from "../payment/payments";
+import { useForm } from "react-hook-form";
 
-const Registration = ({ userAuthentication, setUserAuthentication, userAdmin, userName }) => {
+const dayjs = require("dayjs");
+
+const Registration = () => {
   const navigate = useNavigate();
   const { id, coupon } = useParams();
-  const [registrationInfo, setRegistrationInfo] = useState({
-    user: {},
-    event: {},
-    categories: [],
-  });
-  const { user, event, categories } = registrationInfo;
+  const [user, setUser] = useState(null);
+  const [event, setEvent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [userRegistration, setUserRegistration] = useState({
-    category: "",
-    shirt: "",
-    rules: false,
-  });
+  const {
+    getValues,
+    setError,
+    setValue,
+    resetField,
+    watch,
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
 
-  const [stage, setStage] = useState(1);
+  const fetchEventInfo = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`/api/events/${id}`, {
+        method: "GET",
+      });
+      const parseEvent = await response.json();
+      setEvent(parseEvent);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchUser = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("token", localStorage.token);
+    try {
+      const response = await fetch(`/api/users/self`, {
+        method: "GET",
+        headers: myHeaders,
+      });
+      const parseResponse = await response.json();
+      setUser(parseResponse);
+      console.log(parseResponse);
+      if (parseResponse.type === "error") {
+        toast.error(parseResponse.message, { theme: "colored" });
+        navigate(`/eventos/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchRegistrationAvailability = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("token", localStorage.token);
+    try {
+      const response = await fetch(`/api/registrations/${id}/checkreg/${coupon ? coupon : ""}`, {
+        method: "GET",
+        headers: myHeaders,
+      });
+      const parseResponse = await response.json();
+      if (parseResponse.type === "error") {
+        toast.error(parseResponse.message, { theme: "colored" });
+        navigate(`/eventos/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchRegistration = async () => {
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append("token", localStorage.token);
-      try {
-        const response = await fetch(`/api/registrations/${id}/checkreg/${coupon ? coupon : ""}`, {
-          method: "GET",
-          headers: myHeaders,
-        });
-        const parseResponse = await response.json();
-        if (parseResponse.type === "error") {
-          toast.error(parseResponse.message, { theme: "colored" });
-          navigate(`/eventos/${id}`);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchRegistration().then(
-      fetchInformation(id).then((data) => {
-        setRegistrationInfo(data);
-      })
-    );
-  }, [id, navigate]);
+    fetchEventInfo();
+    fetchRegistrationAvailability();
+    fetchUser();
+  }, []);
 
-  if (registrationInfo.event.event_status === false) {
-    toast.error("As inscrições para este evento não estão abertas.", { theme: "colored" });
-    return <Navigate to="/" />;
-  }
-
-  if (!registrationInfo.categories.length) {
+  if (isLoading) {
     return <LoadingScreen />;
   }
 
@@ -71,72 +104,11 @@ const Registration = ({ userAuthentication, setUserAuthentication, userAdmin, us
       <div className="container inner-page">
         <h1 className="text-center text-justify mb-3">
           Inscrição - <br className="d-block d-lg-none" />
-          {event.event_name}
+          {event?.event_name}
         </h1>
-
-        <div className="container">
-          {/* <div class="position-relative m-4">
-            <div class="progress" style={{ height: "1px" }}>
-              <div class="progress-bar" role="progressbar" style={{ width: "50%" }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <button
-              type="button"
-              class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-primary rounded-pill"
-              style={{ width: "2rem", height: "2rem" }}
-            >
-              1
-            </button>
-            <button
-              type="button"
-              class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-primary rounded-pill"
-              style={{ width: "2rem", height: "2rem" }}
-            >
-              2
-            </button>
-            <button
-              type="button"
-              class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-secondary rounded-pill"
-              style={{ width: "2rem", height: "2rem" }}
-            >
-              3
-            </button>
-          </div> */}
-          <div className="progress my-3" style={{ height: "50px" }}>
-            <div
-              className="progress-bar"
-              role="progressbar"
-              style={{ width: `${stage === 1 ? "50%" : "100%"}` }}
-              aria-valuenow="25"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >
-              <h4>{stage === 1 ? "Atleta" : stage === 2 ? "Inscrição" : "Pesquisa"}</h4>
-            </div>
-          </div>
-        </div>
-        {stage === 1 ? (
-          <UserInfo user={user} registrationInfo={registrationInfo} setRegistrationInfo={setRegistrationInfo} />
-        ) : stage === 2 ? (
-          <EventInfo
-            user={user}
-            event={event}
-            categories={categories}
-            registrationInfo={registrationInfo}
-            setRegistrationInfo={setRegistrationInfo}
-            userRegistration={userRegistration}
-            setUserRegistration={setUserRegistration}
-          />
-        ) : (
-          <Payments id={event.event_id} />
-        )}
-        <StageButtons
-          stage={stage}
-          setStage={setStage}
-          userRegistration={userRegistration}
-          registrationInfo={registrationInfo}
-          id={id}
-          coupon={coupon}
-        />
+        <form>
+          <UserInfo user={user} setValue={setValue} register={register} />
+        </form>
       </div>
     </Fragment>
   );
