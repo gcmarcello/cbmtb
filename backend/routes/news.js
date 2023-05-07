25033e3297e5b8e5152f0b90788fdcc999f5c114
gcmarcello <marcellogcoelho@outlook.com>
2023-05-07 20:16:55 -0300

    feat: added news permissions for press

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -7,10 +7,10 @@ const multer = require("multer");
 var upload = multer({ dest: "uploads/" });
 
 // Create News (ADMIN)
-router.post("/", [adminAuthorization(), upload.single("image")], newsController.create_news);
+router.post("/", [adminAuthorization(["press"]), upload.single("image")], newsController.create_news);
 
 // List News (ADMIN)
-router.get("/", adminAuthorization(), newsController.list_news_admin);
+router.get("/", adminAuthorization(["press"]), newsController.list_news_admin);
 
 // List All News (PUBLIC)
 router.get("/public/all", newsController.list_all_news_public);
@@ -19,18 +19,18 @@ router.get("/public/all", newsController.list_all_news_public);
 router.get("/public/:category?", newsController.list_news_public);
 
 // List Categories
-router.get("/categories", adminAuthorization(), newsController.list_categories);
+router.get("/categories", adminAuthorization(["press"]), newsController.list_categories);
 
 // Publish/Unpublish News (ADMIN)
-router.put("/toggle/:id/:boolean", adminAuthorization(), newsController.publish_unpublish_news);
+router.put("/toggle/:id/:boolean", adminAuthorization(["press"]), newsController.publish_unpublish_news);
 
 // Fetch Specific News (PUBLIC)
 router.get("/:id", newsController.fetch_specific_news);
 
 // Update News (ADMIN)
-router.put("/:id", [adminAuthorization(), upload.single("image")], newsController.update_news);
+router.put("/:id", [adminAuthorization(["press"]), upload.single("image")], newsController.update_news);
 
 // Delete News
-router.delete("/:id", adminAuthorization(), newsController.delete_news);
+router.delete("/:id", adminAuthorization(["press"]), newsController.delete_news);
 
 module.exports = router;
