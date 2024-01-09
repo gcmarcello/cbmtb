4e519454ed109e06da0dd0f85d5122bedf326817
gcmarcello <marcellogcoelho@outlook.com>
2024-01-09 19:50:21 -0300

    Add bucket_name to _config.js

--- backend/_config.js
+++ backend/_config.js
@@ -1,12 +1,22 @@
 const _config = {
+  bucket_name: "cbmtb",
   site: {
-    url: process.env.NODE_ENV === "production" ? "https://cbmtb.com.br" : "http://localhost:3000",
-    reCaptchaSiteKey: process.env.NODE_ENV === "production" ? "6LeMSVslAAAAAIMjysMeMhlXvjiZfWEIxWqqDZN-" : "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+    url:
+      process.env.NODE_ENV === "production"
+        ? "https://cbmtb.com.br"
+        : "http://localhost:3000",
+    reCaptchaSiteKey:
+      process.env.NODE_ENV === "production"
+        ? "6LeMSVslAAAAAIMjysMeMhlXvjiZfWEIxWqqDZN-"
+        : "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
   },
   images: {
-    primaryLogo: "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logonotxt.png",
-    secondaryLogo: "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logoconf_white.svg",
-    dashboardLogo: "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logoconf_black.svg",
+    primaryLogo:
+      "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logonotxt.png",
+    secondaryLogo:
+      "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logoconf_white.svg",
+    dashboardLogo:
+      "https://cbmtb.s3.sa-east-1.amazonaws.com/assets/logoconf_black.svg",
   },
   entidade: {
     abbreviation: "CBMTB",
